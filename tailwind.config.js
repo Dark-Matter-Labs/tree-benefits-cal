1153f2629eae4bd6b64afff9c973f7f882d75597
Gurden Batra <gurden@darkmatterlabs.org>
2026-01-12 11:58:54 +0100

    fix styles issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -75,6 +77,3 @@ const config: Config = {
   },
   plugins: []
 };
-
-export default config;
-
